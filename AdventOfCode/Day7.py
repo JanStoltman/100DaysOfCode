4ef33491b70227139e4d9de34a53fbcfdbf732f1
JanStoltman <stoltmanjan@gmail.com>
2017-12-12 15:54:10 +0100

    Day 9

--- AdventOfCode/Day7.py
+++ AdventOfCode/Day7.py
@@ -1,7 +1,7 @@
 def extract_data(line):
 	data = line.split("->")
 	main = data[0].split(' ')[0]
-	weight = data[0].split(' ')[1].replace(')','').replace('(','')
+	weight = int(data[0].split(' ')[1].replace(')','').replace('(',''))
 	if(len(data) > 1):
 		nodes = map(lambda s: s.rstrip().strip(), data[1].split(","))
 		return main,nodes,weight
@@ -16,13 +16,28 @@ weights = {}
 for line in db_file.readlines():
 	main, nodes, weight = extract_data(line)
 	weights[main] = weight
-	print main
-	print nodes
 	for node in nodes:
 		connections[node] = main
+#--------First part---------------------------
+#search_node, _,_ = extract_data("rmhcw (26)")
+#while connections.get(search_node,'--') != '--':
+#	search_node = connections.get(search_node,'--')
+
+def get_sub_weights(node, connections, weights):
+	sub_nodes = []
+	for k,v in connections.items():
+		if v == node:
+			sub_nodes.append(k)
+	
+
+	sub_weights = [get_sub_weights(n,connections,weights) for n in sub_nodes]
+	if not(sub_weights[1:] == sub_weights[:-1]):
+		print sub_nodes
+		print sub_weights
+
+	return sum(sub_weights) + weights[node]
 
 basis = "ykpsek" #First part answer 
+get_sub_weights(basis,connections, weights)
 
-search_node, _,_ = extract_data("rmhcw (26)")
-while connections.get(search_node,'--') != '--':
-	search_node = connections.get(search_node,'--')
+			
---
+++ AdventOfCode/Day8.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+def readInstructions():
+	db = readFile("Day8DB")
+	tpm = [] 
+	for line in db.readLines()
+	
+
+
+instructions = readInstructions()
