35461d7600bcbf9259ff1b0b25c9fa586a6823f1
JanStoltman <stoltmanjan@gmail.com>
2017-12-26 16:30:20 +0100

    Day 23

--- AdventOfCode/Day22.py
+++ AdventOfCode/Day22.py
@@ -1,4 +1,3 @@
-from itertools import cycle
 def get_data():
 	f = open("Day22DB")
 	db = []
@@ -7,20 +6,80 @@ def get_data():
 	return db
 
 def get_start_pt(db):
-	ys = len(db) - 1 / 2
-	xs = len(db[0]) - 1 / 2
+	ys = (len(db) - 1) / 2
+	xs = (len(db[0]) - 1) / 2
 	return ys, xs
 
+def turn_left(b):
+	return 4 if b == 1 else (b - 1)
+
+def turn_right(b):
+	return 1 if b == 4 else (b + 1)
+
+def move(y, x, b):
+	if   b == 1:
+		y -= 1
+	elif b == 2:
+		x += 1
+	elif b == 3:
+		y += 1
+	elif b == 4:
+		x -= 1
+
+	return y, x 
+
 db     = get_data()
-b      = cycle([1,2,3,4]) #bearing 1-U 2-R 3-D 4-L
-i, j = get_start_pt(db)
+b      = 1 #bearing 1-U 2-R 3-D 4-L
+y, x = get_start_pt(db)
 inf    = 0#How many infected cells
 
-for _ in range(0,1000):
+for _ in range(0,10000000):
+	if y < 0:
+		db.insert(0,(['.'] * len(db[0])))
+		y += 1
+	elif y >= len(db):
+		db.append(['.'] * len(db[0]))
+	elif x < 0:
+		db = [['.'] + r for r in db]
+		x += 1
+	elif x >= len(db[0]):
+		db = [r + ['.'] for r in db]
+
 	c = db[y][x]
-	
+
 	if   c == '.':
-		b = next(b)	
+		b = turn_left(b)
+		db[y][x] = 'w'	
 	elif c == '#':
-		
+		b = turn_right(b)	
+		db[y][x] = 'f'
+	elif c == 'w':
+		db[y][x] = '#'	
+		inf += 1
+	elif c == 'f':
+		b = turn_left(b)
+		b = turn_left(b)
+		db[y][x] = '.'	
 	
+	y, x = move(y, x, b)
+
+print inf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AdventOfCode/Day23.py
@@ -0,0 +1,72 @@
+from collections import deque 
+
+f     = open("Day23DB")
+lines = f.readlines()
+f.close()
+
+regs  = {}
+i     = 0
+s     = 0
+
+while i >= 0 and i < len(lines):
+	t = map(lambda x: x.strip(), lines[i].split(' '))
+
+	if t[0] == "set": 
+		if t[2].isalpha():
+			regs[t[1]] = regs.get(t[2], 0)
+		else:
+			regs[t[1]] = int(t[2])
+	elif t[0] == "sub": 
+		if t[2].isalpha():
+			regs[t[1]] = regs.get(t[1], 0) - regs.get(t[2],0)
+		else:
+			regs[t[1]] = regs.get(t[1], 0) - int(t[2])
+
+	elif t[0] == "mul": 
+		s += 1
+		if t[2].isalpha():
+			regs[t[1]] = regs.get(t[1], 0) * regs.get(t[2], 0)
+		else:
+			regs[t[1]] = regs.get(t[1], 0) * int(t[2])
+	
+	elif t[0] == "jnz": 
+		if t[1].isalpha():
+			if regs.get(t[1], 0) != 0:
+				if t[2].isalpha():
+					i += regs.get(t[2],0) - 1
+				else:
+					i += int(t[2]) - 1
+		else:
+			if int(t[1]) != 0:
+				if t[2].isalpha():
+					i += regs.get(t[2],0) - 1
+				else:
+					i += int(t[2]) - 1
+	
+	i += 1
+
+print s
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
