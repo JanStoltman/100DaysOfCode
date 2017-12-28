abe4ce801640d12e679dc4289516d59bda92808a
JanStoltman <stoltmanjan@gmail.com>
2017-12-28 09:07:16 +0100

    Day 25 -- break for hichhiking through Europe

--- AdventOfCode/Day24.py
+++ AdventOfCode/Day24.py
@@ -9,3 +9,16 @@ for l in f.readlines():
 
 cm = sorted(cm, key=sum, reverse=True)
 
+trees = []
+
+i = 0
+while i < len(cm):
+	if cm[i][1] == 0 or cm[i][0] == 0:
+		t = Tree()
+		t.data = ((cm[i][0],cm[i][1], 1))		
+		trees.append(t)
+		del(cm[i])
+	else:
+		i += 1
+
+
--- AdventOfCode/Tree.py
+++ AdventOfCode/Tree.py
@@ -1,5 +1,4 @@
 class Tree(object):
 	def __init__(self):
 		self.right = None
-		self.left  = None
-		self.data  = None
+		self.nodes = []
