cb26772e6eece08527606dd6387f230a876edf9e
JanStoltman <stoltmanjan@gmail.com>
2018-01-13 20:25:05 +0100

    Day 28

--- AdventOfCode/Day24.py
+++ AdventOfCode/Day24.py
@@ -1,4 +1,24 @@
 from Tree import TreeNode
+def zdir(root):
+	if   root[0] == 0:
+		return 0 #left
+	elif root[1] ==0:
+		return 1 #right
+	else:
+		return 2 #nothing
+
+
+
+def add_nodes(root, nodes):
+	for i in range(0,len(nodes)):
+		if nodes[i][0] == root.gfv() or nodes[i][0] == root.gfv():
+			root.addChild(nodes[i])
+
+	return root
+			
+	
+
+
 f  = open("Day24DB")
 cm = [] 
 
@@ -6,8 +26,13 @@ for l in f.readlines():
 	t = l.split('/')
 	cm.append((int(t[0]), int(t[1]), 0))
 
-
 cm   = sorted(cm, key=sum, reverse=True)
 begs = filter(lambda x:     x[0] == 0 or x[1] == 0 , cm) 
 cm   = filter(lambda x: not(x[0] == 0 or x[1] == 0), cm) 
+rts  =    map(lambda x: TreeNode(x, zdir(x)), begs)
+t    = []
 
+for root in rts:
+	t.append(add_nodes(root, filter(lambda x: not(x[0] == root.l and x[1] == root.r), cm)))
+
+print t
--- AdventOfCode/Tree.py
+++ AdventOfCode/Tree.py
@@ -1,7 +1,19 @@
-class TreeNode():
-     def __init__(self,initWeight=0):
-        self.children = List()
-        self.weight = initWeight
 
-	def addChild(self, weight):
-		self.children.append(TreeNode(weight))
+class TreeNode():
+	def __init__(self, x, d=0):
+		self.l = x[0]
+		self.r = x[1]
+		self.a = d
+		self.children = []
+		self.weight   = x[0] + x[1]
+
+	def addChild(self, x):
+		self.children.append(TreeNode(x))
+
+	def gfv(self):
+		if   self.a == 0:
+			return self.l
+		elif self.a == 1:
+			return self.l
+		else:
+			raise Error("unexpected val")
