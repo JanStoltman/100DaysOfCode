0a1bc6a0a8a83f584c3bd9749ccbdc3c1d844403
JanStoltman <stoltmanjan@gmail.com>
2018-01-18 11:01:09 +0100

    Day 32

--- Haskell/MangaScrapper/App.hs
+++ Haskell/MangaScrapper/App.hs
@@ -2,33 +2,51 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module App where
 
-import Control.Monad.Trans.Except
 import Data.Aeson
 import GHC.Generics
-import Network.Wai
 import Network.Wai.Handler.Warp
+import Data.Text
+import Data.List
 import Servant
 import Servant.HTML.Lucid
+import Network.HTTP.Media
 import System.IO
+import Lucid
 import Scrapper
 import Network.Wreq
 import Control.Monad.IO.Class
 
--- * api
+data HTMLLucid
+instance Servant.Accept HTMLLucid where
+    contentType _ = "text" // "html" /: ("charset", "utf-8")
+instance ToHtml a => MimeRender HTMLLucid a where
+    mimeRender _ = renderBS . toHtml
+instance MimeRender HTMLLucid (Html a) where
+    mimeRender _ = renderBS
+instance ToHtml (String,String) where 
+  toHtml (n, l) = 
+    tr_ $ do
+      td_ $ do
+       a_ [href_ $ pack l] (toHtml $ n)
+  toHtmlRaw = toHtml
+
 data TitleList = TitleList 
   { titles :: [(String,String)]
   } deriving (Show, Eq, Generic)
 
 instance ToJSON TitleList
 instance ToHtml TitleList where
-  toHtml titleList =
-    table_ $ do
-      tr_ $ do
-        td_ (toHtml $ titles titleList)
-
+  toHtml list = table_ $ do
+    tr_ $ do
+      th_ "Mangas"
+    foldMap toHtml $ Prelude.map ft $ titles list
+      where ft (a, b) = (a, "/chapters" ++ b)  
   toHtmlRaw = toHtml
 
 data ChapterList = ChapterList 
@@ -36,31 +54,65 @@ data ChapterList = ChapterList
   } deriving (Show, Eq, Generic)
 
 instance ToJSON ChapterList
+instance ToHtml ChapterList where
+  toHtml list = table_ $ do
+    tr_ $ do
+      th_ "Chapters"
+    foldMap toHtml $ Prelude.map fc $ chapters list
+      where fc (a, b) = (a, "/page" ++ b ++ "/1")
+  toHtmlRaw = toHtml
 
 data Photo = Photo
-  { info :: (String, String, String, String)
+  { info :: (String, String, String, String, String)
   } deriving (Show, Eq, Generic)
 
 instance ToJSON Photo
+instance ToHtml Photo where
+  toHtml (Photo (w, h, s, a, l)) = do
+    a_ [href_ "javascript:history.go(-1)"] "Prev"
+    img_ [src_ $ pack s]
+    a_ [href_ $ gns l] "Next" 
+  toHtmlRaw = toHtml
 
-type MangaAPI = "titles"   :> Get '[JSON, HTMLLucid] TitleList 
-           :<|> "chapters" :> Capture "title"  String :> Get '[JSON] ChapterList
-           :<|> "page"     :> Capture "title"  String :> Capture "chapter" String :> Capture "page" String :>  Get '[JSON] Photo
+gns :: String -> Text
+gns s = pack $ show $ l + 1
+  where 
+    l = read $ unpack $ Prelude.last $ splitOn "/" $ pack s
 
+
+
+
+
+
+
+type MangaAPI = "titles"   :> Get '[HTMLLucid] TitleList 
+           :<|> "chapters" :> Capture "title"  String :> Get '[HTMLLucid] ChapterList
+           :<|> "page"     :> Capture "title"  String :> Capture "chapter" String :> Capture "page" String :>  Get '[HTMLLucid] Photo
+
+tls :: IO TitleList
 tls = do
   ts <- get "http://www.mangareader.net/alphabetical"
   return $ TitleList $ Scrapper.getTitles ts
   
+cps :: String -> IO ChapterList
 cps n = do
   ts <- get ("http://www.mangareader.net/" ++ n)
   return $ ChapterList $ Scrapper.getChapters ts
 
+img :: String -> IO Photo
 img n = do 
-  ts <- get ("http://www.mangareader.net/" ++ n)
-  return $ Photo $ Scrapper.getImage ts
+  ts <- get l
+  return $ Photo $ Scrapper.getImage ts n
+  where 
+   l = "http://www.mangareader.net/" ++ n
 
+gt :: Handler TitleList
 gt = liftIO $ tls 
+
+gc :: MonadIO m => [Char] -> m ChapterList
 gc a = liftIO $ cps a
+
+gi :: MonadIO m => [Char] -> [Char] -> [Char] -> m Photo
 gi t c p = liftIO $ img $ (t ++ "/" ++ c ++ "/" ++ p)
 
 server :: Server MangaAPI
--- Haskell/MangaScrapper/Scrapper.hs
+++ Haskell/MangaScrapper/Scrapper.hs
@@ -10,11 +10,16 @@ import Control.Lens
 import Data.ByteString.Lazy.Char8 as Char8
 import Data.Maybe
 
+getTitles :: Response ByteString -> [([Char], [Char])]
 getTitles r = Prelude.concat $ fromJust $ scalpTitles $ Char8.unpack $ r ^. responseBody
+
+getChapters :: Response ByteString -> [([Char], [Char])]
 getChapters r = Prelude.concat $ fromJust $ scalpChapters $ Char8.unpack $ r ^. responseBody
-getImage r = fromJust $ scalpImage $ Char8.unpack $ r ^. responseBody
 
+getImage :: Response ByteString -> t -> ([Char], [Char], [Char], [Char], t)
+getImage r l = fromJust $ scalpImage (Char8.unpack $ r ^. responseBody) l
 
+scalpTitles :: String -> Maybe [[(String, String)]]
 scalpTitles s = scrapeStringLike s gt
   where
     gt = chroots ("div"//"ul" @: [hasClass "series_alpha"])$ do 
@@ -22,6 +27,7 @@ scalpTitles s = scrapeStringLike s gt
       li <- attrs "href" "a"
       return $ Prelude.zip ti li
 
+scalpChapters :: String -> Maybe [[(String, String)]]
 scalpChapters s = scrapeStringLike s gt
   where
     gt = chroots ("div" @: [AnyAttribute @= "chapterlist"])$ do 
@@ -29,14 +35,15 @@ scalpChapters s = scrapeStringLike s gt
       li <- attrs "href" "a"
       return $ Prelude.zip ti li 
 
-scalpImage ss = scrapeStringLike ss gt
+scalpImage :: String -> t -> Maybe (String, String, String, String, t)
+scalpImage ss l = scrapeStringLike ss gt
   where
     gt = chroot ("div") $ do 
       w <- attr "width"  "img"
       h <- attr "height" "img"
       s <- attr "src"    "img"
       a <- attr "alt"    "img"
-      return (w, h, s, a)
+      return (w, h, s, a, l)
 
 
 
