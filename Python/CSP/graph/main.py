52e0244a1ecdd1800c45a60c9f8ae692d485b6b2
Jan Stoltman <stoltmanjan@gmail.com>
2018-03-28 16:18:24 +0200

    Day 100

--- Python/CSP/graph/main.py
+++ Python/CSP/graph/main.py
@@ -1,22 +1,57 @@
 import time
 
 N = 5
-graph = [[-1]*N]*N
+graph = [[-1 for _ in range(0,N)] for _ in range(0,N)]
 
 def print_graph():
     for i in range(0, len(graph)):
         print(str(graph[i]) + '\n')
 
+
+def ch_l(r, c, val):
+    return c == 0 or abs(graph[r][c - 1] - val) >= 2
+
+
+def ch_u(r, c, val):
+    return r == 0 or abs(graph[r - 1][c] - val) >= 2
+
+def ch_cu(r, c, val):
+    return c == 0 or r == 0 or abs(graph[r - 1][c - 1] - val) >= 1
+
+def ch_cd(r, c, val):
+    return r == 0 or r == N - 1 or abs(graph[r + 1][c - 1] - val) >= 1
+
+
+def check_cond(r, c):
+    """
+    :param r: Row index
+    :param c: Column index
+    :return: True if color meets all constraints
+    """
+    if graph[r][c] == -1:
+        return False
+
+    val = graph[r][c]
+    return ch_l(r,c,val) and ch_u(r,c,val) and ch_cd(r,c,val) and  ch_cu(r,c,val)
+
+
 def backtracking():
-    pass
+    for c in range(0,N):
+        for r in range(0,N):
+            col = 0
+            while not check_cond(r,c):
+                graph[r][c] = col
+                col += 1
+
+    print_graph()
 
 def fowardchecking():
     pass
 
 def main():
     print_graph()
-
     st = time.time()
+    backtracking()
     en = time.time()
     print(str(en-st))
 
